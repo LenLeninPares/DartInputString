5228aef829990c8110d9b8de421a960bb4c31d5b
Maggyy_Magz <169112174+LenLeninPares@users.noreply.github.com>
2025-08-14 20:35:31 +0800

    Dart Input String Validation

---
+++ Act4.dart
@@ -0,0 +1,43 @@
+// ACT 4
+// import 'dart:io';
+
+// void main() {
+//     print("Enter your name ");
+//     String? name = stdin.readLineSync();
+//     print("your name is ${name}");
+
+//     if (name != null && name.isNotEmpty) {
+//      print("Nice!!!");
+
+//     } else {
+//      print("nagbabasa ka ba??");
+//     }
+
+//     print("Enter your number ");
+//     String? number = stdin.readLineSync();
+
+//     if (number != null && number.isNotEmpty) {
+//         print("SUCCESS");
+//     } else {
+//         print("KUPAL WALA KANG NILAGAY");
+//     }
+
+//     print("Enter your email ");
+//     String? mail = stdin.readLineSync();
+
+//     if (mail != null && mail.contains("@gmail") && mail.isNotEmpty) {
+//         print("SUCCESS");
+//     } else {
+//         print("KUPAL WALA KANG @");
+//     }
+
+// }
+// import 'dart:io';
+
+// void main() {
+//   print("Insert your number:");
+//   double? number = double.parse(stdin.readLineSync()!);
+//   print("your number is ${number}");
+// }
+
+// ACT 5
